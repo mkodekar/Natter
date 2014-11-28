ecb05ded181b63c23bf950bc7d2115dc34d49a69
Alexander Prime <rainesabaal@gmail.com>
2014-11-28 12:26:59 -0700

    Fix casting crash on receiving an SMS

--- app/src/main/java/cf/substance/natter/receiver/SmsReceiver.java
+++ app/src/main/java/cf/substance/natter/receiver/SmsReceiver.java
@@ -13,10 +13,10 @@ public class SmsReceiver extends BroadcastReceiver {
 	@Override public void onReceive( Context context, Intent intent ) {
 		if ( !Telephony.Sms.Intents.SMS_DELIVER_ACTION.equals( intent.getAction() )) return;
 
-		byte[][] pdus = (byte[][])intent.getExtras().get( "pdus" );
+		Object[] pdus = (Object[])intent.getExtras().get( "pdus" );
 		SmsMessage[] messages = new SmsMessage[pdus.length];
 		for ( int i = 0; i < messages.length; i++ ) {
-			messages[i] = SmsMessage.createFromPdu( pdus[i] );
+			messages[i] = SmsMessage.createFromPdu( (byte[])pdus[i] );
 		}
 
 		onSmsReceived( context, messages );
